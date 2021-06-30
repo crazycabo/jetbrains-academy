b0ff94f7b71079ded315e85e814f51a74c1d9a90
Brian Smith <crazycabo350@gmail.com>
2021-06-30 11:01:28 -0400

    Create Contact class to hold a person info

---
+++ Contacts1/Contacts/task/src/contacts/Contact.java
@@ -0,0 +1,35 @@
+package contacts;
+
+/**
+ * Created by Brian Smith on 6/30/21.
+ * Description:
+ */
+public class Contact {
+    String name;
+    String surname;
+    String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
