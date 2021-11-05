068679a8a3fcaf84a76cf74222d6199d700dad2f
Brian Smith <crazycabo350@gmail.com>
2021-11-05 10:38:23 -0400

    Complete stage 1

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,21 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Secret code is 0101
+        printMessage("The secret code is prepared: ****.\n");
+
+        printMessage("Turn 1. Answer:");
+        printMessage("0000");
+        printMessage("Grade: 2 cows.");
+
+        printMessage("Turn 1. Answer:");
+        printMessage("0101");
+        printMessage("Grade: 4 bulls.");
+
+        printMessage("Congrats! The secret code is 0101.");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 }
