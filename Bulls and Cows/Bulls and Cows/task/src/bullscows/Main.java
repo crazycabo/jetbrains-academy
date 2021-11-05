20f4a9ea5c60c56be9b37931d2cb27fddbf24783
Brian Smith <crazycabo350@gmail.com>
2021-11-05 13:00:59 -0400

    Complete stage 2

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -1,19 +1,56 @@
 package bullscows;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    static String secretCode = generateNewCode();
+
     public static void main(String[] args) {
-        // Secret code is 0101
-        printMessage("The secret code is prepared: ****.\n");
+        Scanner scanner = new Scanner(System.in);
 
-        printMessage("Turn 1. Answer:");
-        printMessage("0000");
-        printMessage("Grade: 2 cows.");
+        if (scanner.hasNext()) {
+            processUserCode(scanner.next());
+        }
+    }
 
-        printMessage("Turn 1. Answer:");
-        printMessage("0101");
-        printMessage("Grade: 4 bulls.");
+    private static void processUserCode(String code) {
+        int bullCount = 0;
+        int cowCount = 0;
+        List<Integer> inputCowDigits = new ArrayList<>();
+        List<Integer> secretCodeCowDigits = new ArrayList<>();
 
-        printMessage("Congrats! The secret code is 0101.");
+        for (int i = 0; i < code.length(); i++) {
+            if (secretCode.charAt(i) == code.charAt(i)) {
+                bullCount++;
+            } else {
+                inputCowDigits.add(Character.getNumericValue(code.charAt(i)));
+                secretCodeCowDigits.add(Character.getNumericValue(secretCode.charAt(i)));
+            }
+        }
+
+        for (Integer digit : inputCowDigits) {
+            if (secretCodeCowDigits.contains(digit)) {
+                cowCount++;
+                secretCodeCowDigits.remove(digit);
+            }
+        }
+
+        if (bullCount + cowCount == 0) {
+            printMessage("Grade: None. The secret code is " + secretCode);
+        } else {
+            printMessage("Grade: " +
+                    ((bullCount > 0) ? bullCount + " bull(s)" : "") +
+                    ((cowCount > 0) ? cowCount + " cow(s)" : "") +
+                    ". The secret code is " + secretCode);
+        }
+    }
+
+    private static String generateNewCode() {
+        double d = Math.floor(Math.random() * (9999 - 1000) + 1);
+        return String.valueOf((int) d);
     }
 
     private static void printMessage(String message) {
