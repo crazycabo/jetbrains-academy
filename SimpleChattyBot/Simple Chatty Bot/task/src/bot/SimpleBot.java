29ded07b5279166a24eacfb568e163fdc3776cb7
Brian Smith <crazycabo350@gmail.com>
2021-11-06 13:22:08 -0400

    Complete stage 1

---
+++ SimpleChattyBot/Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,12 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        printMessage("Hello! My name is Sparticus.");
+        printMessage("I was created in 2021.");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
