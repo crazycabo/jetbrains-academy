a0d1add1644de4eac2be4d2e967098dd47389c12
Brian Smith <crazycabo350@gmail.com>
2021-11-06 21:17:19 -0400

    Create some initial classes and methods

--- Battleship/Battleship/task/src/battleship/Main.java
+++ Battleship/Battleship/task/src/battleship/Main.java
@@ -3,6 +3,11 @@ package battleship;
 public class Main {
 
     public static void main(String[] args) {
-        // Write your code here
+        // todo: set up game board
+        GameState gameState = new GameState();
+
+        gameState.setPlayerShips();
+
+        // todo: enter locations of each of your ships
     }
 }
---
+++ Battleship/Battleship/task/src/battleship/UtilityFunctions.java
@@ -0,0 +1,16 @@
+package battleship;
+
+/**
+ * Created by Brian Smith on 11/6/21.
+ * Description:
+ */
+public class UtilityFunctions {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printNewline() {
+        System.out.print("\n");
+    }
+}
